8abd4e589326cc866d0ed253ca78005d400b9698
Patrick McDonald <paddymcdonald@gmail.com>
2022-06-16 00:55:05 +0100

    Call Microsft.Graph from app Refactor Program.cs Add Razor runtime compilation Fix Layout style

--- MathsQuizApp/Controllers/HomeController.cs
+++ MathsQuizApp/Controllers/HomeController.cs
@@ -2,14 +2,27 @@
 using MathsQuizApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
 
 namespace MathsQuizApp.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
-        public ActionResult Index()
+        private readonly GraphServiceClient _graphServiceClient;
+    
+        public HomeController(GraphServiceClient graphServiceClient)
         {
+            _graphServiceClient = graphServiceClient;
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "MicrosoftGraph:Scopes")]
+        [HttpGet]
+        public async Task< ActionResult> Index()
+        {
+            var user = await _graphServiceClient.Me.Request().GetAsync();
+            ViewData["GraphApiResult"] = user.DisplayName;
+            
             return View();
         }
 
--- MathsQuizApp/Program.cs
+++ MathsQuizApp/Program.cs
@@ -6,63 +6,79 @@ using Microsoft.Identity.Web.UI;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var services = builder.Services;
-var configuration = builder.Configuration;
 
-services.Configure<CookiePolicyOptions>(options =>
-{
-    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-    options.CheckConsentNeeded = _ => true;
-    options.MinimumSameSitePolicy = SameSiteMode.None;
-    // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/core/security/samesite?view=aspnetcore-3.1
-    options.HandleSameSiteCookieCompatibility();
-});
-
-var initialScopes = configuration.GetValue<string>("DownstreamApi:Scopes")?.Split(' ');
-
-services.AddMicrosoftIdentityWebAppAuthentication(configuration)
-    .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
-    .AddMicrosoftGraph(configuration.GetSection("DownstreamApi"))
-    .AddDownstreamWebApi("MathsQuizApi", configuration.GetSection("MathsQuizApi"))
-    .AddInMemoryTokenCaches();
-
-services.AddControllersWithViews(options =>
-{
-    var policy = new AuthorizationPolicyBuilder()
-                .RequireAuthenticatedUser()
-                .Build();
-    options.Filters.Add(new AuthorizeFilter(policy));
-});
-
-services.AddRazorPages()
-    .AddMicrosoftIdentityUI();
+ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapDefaultControllerRoute();
-    endpoints.MapRazorPages();
-});
+ConfigureMiddleware(app, app.Environment);
+ConfigureEndpoints(app);
 
 app.Run();
+
+static void ConfigureServices(IServiceCollection services, ConfigurationManager configuration)
+{
+    services.Configure<CookiePolicyOptions>(options =>
+    {
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = _ => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+        // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/core/security/samesite?view=aspnetcore-3.1
+        options.HandleSameSiteCookieCompatibility();
+    });
+
+    var initialScopes = configuration.GetValue<string>("DownstreamApi:Scopes")?.Split(' ');
+
+    services.AddMicrosoftIdentityWebAppAuthentication(configuration)
+        .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+        .AddMicrosoftGraph(configuration.GetSection("MicrosoftGraph"))
+        .AddDownstreamWebApi("MathsQuizApi", configuration.GetSection("MathsQuizApi"))
+        .AddInMemoryTokenCaches();
+
+    services.AddAuthorization(options =>
+    {
+        // By default, all incoming requests will be authorized according to the default policy.
+        options.FallbackPolicy = options.DefaultPolicy;
+    });
+
+    services.AddControllersWithViews(options =>
+    {
+        var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+    });
+
+    services.AddRazorPages()
+        .AddMicrosoftIdentityUI();
+}
+
+static void ConfigureEndpoints(IEndpointRouteBuilder app)
+{
+    app.MapDefaultControllerRoute();
+    app.MapRazorPages();
+    app.MapControllers();
+}
+
+static void ConfigureMiddleware(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    // Configure the HTTP request pipeline.
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+}
