3083e069bd6264c651dc0a26c5f1b94fa8c8567a
Patrick McDonald <paddymcdonald@gmail.com>
2022-06-16 00:11:48 +0100

    Refactor Program.cs

--- MathsQuiz/Program.cs
+++ MathsQuiz/Program.cs
@@ -2,61 +2,77 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1",
-        new OpenApiInfo
-        {
-            Title = "Maths Quiz API",
-            Version = "0.1"
-        });
-
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-
-    options.EnableAnnotations();
-});
+ConfigureConfiguration(builder.Configuration);
+ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
-{
-    app.UseSwagger(options =>
-    {
-        options.PreSerializeFilters.Add((swagger, httpReq) =>
-        {
-            swagger.Servers = new[]
-            {
-                new OpenApiServer
-                {
-                    Url = $"{httpReq.Scheme}://{httpReq.Host.Value}",
-                    Description = "host",
-                },
-            };
-        });
-    });
+ConfigureMiddleware(app, app.Environment);
+ConfigureEndpoints(app);
 
-    app.UseSwaggerUI(options =>
+app.Run();
+
+void ConfigureConfiguration(ConfigurationManager configuration) { }
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+
+    services.AddSwaggerGen(options =>
     {
-        options.DocumentTitle = "Maths Quiz API - Swagger UI";
-        options.RoutePrefix = "swagger";
-        options.SwaggerEndpoint("v1/swagger.json", "Maths Quiz API V1");
+        options.SwaggerDoc("v1",
+            new OpenApiInfo
+            {
+                Title = "Maths Quiz API",
+                Version = "0.1"
+            });
+
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+        options.EnableAnnotations();
     });
 }
 
-app.UseHttpsRedirection();
+void ConfigureMiddleware(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    // Configure the HTTP request pipeline.
+    if (env.IsDevelopment() || env.IsStaging())
+    {
+        app.UseSwagger(options =>
+        {
+            options.PreSerializeFilters.Add((swagger, httpReq) =>
+            {
+                swagger.Servers = new[]
+                {
+                    new OpenApiServer
+                    {
+                        Url = $"{httpReq.Scheme}://{httpReq.Host.Value}",
+                        Description = "host",
+                    },
+                };
+            });
+        });
 
-app.UseAuthorization();
+        app.UseSwaggerUI(options =>
+        {
+            options.DocumentTitle = "Maths Quiz API - Swagger UI";
+            options.RoutePrefix = "swagger";
+            options.SwaggerEndpoint("v1/swagger.json", "Maths Quiz API V1");
+        });
+    }
 
-app.MapGet("/", request => request.Response.RedirectTemporaryAsync("./swagger/index.html"));
+    app.UseHttpsRedirection();
 
-app.MapControllers();
+    app.UseAuthorization();
 
-app.Run();
+}
+
+void ConfigureEndpoints(IEndpointRouteBuilder app)
+{
+    app.MapGet("/", (HttpResponse response) => response.RedirectTemporaryAsync("./swagger/index.html"));
+
+    app.MapControllers();
+}
